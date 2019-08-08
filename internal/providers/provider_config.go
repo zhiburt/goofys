fd6f014f0aa8f45b77de75e5f4b091bd87bf52c4
Maxim Zhiburt <maxim.zhiburt@idt.net>
2019-08-08 13:10:18 +0300

    Rename file

---
+++ internal/providers/provider_config.go
@@ -0,0 +1,64 @@
+package providers
+
+import (
+	"net/http"
+	"time"
+)
+
+// A VaultConfig stores values for vaultConfigProvider
+//
+// expiredTime is a duration after which time vaultConfigProvider updates secretes
+type VaultConfig struct {
+	expiredTime    time.Duration
+	token          string
+	pathToSecrets  string
+	accessVaultKey string
+	secretVaultKey string
+	url            string
+	client         *http.Client
+}
+
+// ProviderConfig returns config with some default fields
+func ProviderConfig(token, pathToSecrets, accessVaultKey, secretVaultKey, address string) VaultConfig {
+	cfg := VaultConfig{}.
+		SetInformation(token, pathToSecrets, accessVaultKey, secretVaultKey).
+		SetEndpoint(address).
+		SetTimeExperation(6 * time.Second).
+		SetClient(&http.Client{
+			Timeout: 5 * time.Second,
+		})
+
+	return cfg
+}
+
+// SetClient renew new client to cfg
+func (cfg VaultConfig) SetClient(client *http.Client) VaultConfig {
+	cfg.client = client
+
+	return cfg
+}
+
+// SetEndpoint renovate new endpoint to cfg
+func (cfg VaultConfig) SetEndpoint(addr string) VaultConfig {
+	cfg.url = addr
+
+	return cfg
+}
+
+// SetInformation renovate information about vault
+func (cfg VaultConfig) SetInformation(token, pathToSecrets, accessVaultKey, secretVaultKey string) VaultConfig {
+	cfg.pathToSecrets = pathToSecrets
+	cfg.token = token
+
+	cfg.accessVaultKey = accessVaultKey
+	cfg.secretVaultKey = secretVaultKey
+
+	return cfg
+}
+
+// SetTimeExperation renovate expiredTime to cfg
+func (cfg VaultConfig) SetTimeExperation(expiredTime time.Duration) VaultConfig {
+	cfg.expiredTime = expiredTime
+
+	return cfg
+}
